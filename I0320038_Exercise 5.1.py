d9c6c7b2a59e412b96ac5e81d0f7486b84bd3be1
fadhiladiahap <fadhiladiah.ayupratiwi@gmail.com>
2021-03-31 10:24:41 +0700

    TUGAS 5

---
+++ I0320038_Exercise 5.1.py
@@ -0,0 +1,7 @@
+# Penggunaan if untuk satu kasus
+# input bilangan
+bilangan = int(input('Masukan bilangan bulat: '))
+# memeriksa bilangan
+if bilangan %2 == 0:
+    print ('%d adalah bilangan genap' % bilangan)
+print ()
